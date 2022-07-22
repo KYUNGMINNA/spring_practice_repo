c01b230a39a1bbf224a7bc4d908a9acfcd1aa510
KYWOOK <109258605+KYWOOK@users.noreply.github.com>
2022-07-22 19:40:39 +0900

    f file mod (#4)

--- src/main/java/com/spring/prac/f/f.java
+++ src/main/java/com/spring/prac/f/f.java
@@ -1,5 +1,15 @@
 package com.spring.prac.f;
 
-public class f {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class f {
+   
+	@GetMapping("/address")
+	   public String a_method() {
+		return "fff";
+	}
+	
+	
 }
