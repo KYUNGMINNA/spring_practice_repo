721f0a2be0885a81b0cb985c8c0eac57671941d4
kkkmmmyyyy <cityray@naver.com>
2022-07-22 18:04:05 +0900

    testmessage

--- src/main/java/com/spring/prac/c/c.java
+++ src/main/java/com/spring/prac/c/c.java
@@ -1,5 +1,15 @@
 package com.spring.prac.c;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class c {
 
+	
+	@GetMapping("caa")
+	public String methodtest() {
+		
+		return "aaa";
+	}
 }
