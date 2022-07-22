d61785bf38e22844015bdb1885b030b628270c0a
egoing <vkdlfsms@gmail.com>
2022-07-22 21:06:45 +0900

    prac b

--- src/main/java/com/spring/prac/b/b.java
+++ src/main/java/com/spring/prac/b/b.java
@@ -1,5 +1,13 @@
 package com.spring.prac.b;
 
-public class b {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class b {
+	
+	@GetMapping("/address")
+	public String bt_method() {
+		return "aaa";
+	}
 }
