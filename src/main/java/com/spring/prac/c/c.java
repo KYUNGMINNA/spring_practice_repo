7992332fbc7e465cba29bdb34d2188f3bbddf243
kkkmmmyyyy <cityray@naver.com>
2022-07-22 18:32:51 +0900

    sts

--- src/main/java/com/spring/prac/c/c.java
+++ src/main/java/com/spring/prac/c/c.java
@@ -1,5 +1,15 @@
 package com.spring.prac.c;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class c {
 
+	
+	@GetMapping("add")
+	public String met() {
+		return "aaa";
+	}
+	
 }
