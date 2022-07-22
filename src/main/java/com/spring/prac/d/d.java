54795476d6c59ed8b0979b8379e0c27967e0a5ac
crisine <mikunyang@gmail.com>
2022-07-22 19:40:24 +0900

    2022-07-22 풀 리퀘스트 요청입니다 (#3)

--- src/main/java/com/spring/prac/d/d.java
+++ src/main/java/com/spring/prac/d/d.java
@@ -1,5 +1,18 @@
 package com.spring.prac.d;
 
-public class d {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class d {
+	
+	private int studentId;
+	private String studentName;
+	private String major;
+	
 }
