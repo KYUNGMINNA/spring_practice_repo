ab305702f1359d7e61f8304f5df2036dd6586362
KYUNGMIN <62867182+KYUNGMINNA@users.noreply.github.com>
2022-07-23 17:01:19 +0900

    Revert "adasdad (#7)"
    This reverts commit 15308bf72d7375cdcbe507dccaa06ab1d4e5fa95.

--- src/main/java/com/spring/prac/c/c.java
+++ src/main/java/com/spring/prac/c/c.java
@@ -1,13 +1,5 @@
 package com.spring.prac.c;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class c {
 
-	@GetMapping("ads")
-	public String ssss() {
-		return "aaa";
-	}
 }
